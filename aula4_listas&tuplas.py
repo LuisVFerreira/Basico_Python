efd8b54d5d1ac8a264400b5faada0f9e3aee559e
LuisVFerreira <luisvitorio123@gmail.com>
2020-03-25 13:32:28 -0300

    Adicona aula 4

---
+++ aula4_listas&tuplas.py
@@ -0,0 +1,20 @@
+# Listas & Tuplas
+
+Animais = ['gato', 'cachorro', 'leão', "papagaio"]
+
+Inteiros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Imprime elementos utilizanod "for"
+
+for y in Animais:
+    print(y)
+    
+# Imprime a soma da lista de "Inteiros"
+
+print(sum(Inteiros))
+
+# >>> Tupla
+
+Materiais = ('lapis', 'caneta', 'estojo', 'apagador', 'tesoura')
+
+print(Materiais)
